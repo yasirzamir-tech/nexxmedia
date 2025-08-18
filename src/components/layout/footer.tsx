c8492078e1de573c5d82649c28424127ef2bb2ea
Yasir zamir <yasirzamir6@gmail.com>
2025-08-18 20:05:00 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <a href="#" target

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -44,7 +44,7 @@ export default function Footer() {
                 <a href="https://www.instagram.com/nexx.media/" target="_blank" rel="noopener noreferrer" aria-label="Instagram" className="instagram-gradient-hover"><Instagram className="h-5 w-5" /></a>
               </Button>
               <Button variant="ghost" size="icon" asChild>
-                <a href="#" target="_blank" rel="noopener noreferrer" aria-label="Facebook"><Facebook className="h-5 w-5" /></a>
+                <a href="#" target="_blank" rel="noopener noreferrer" aria-label="Facebook" className="facebook-hover"><Facebook className="h-5 w-5" /></a>
               </Button>
             </div>
           </div>
